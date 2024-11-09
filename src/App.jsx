573c2e9c075204704f664fd7fb4c4b40a9c15117
programmershakib <programmershakibinfo@gmail.com>
2024-11-09 14:19:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from 'react-helmet-async'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Helmet>
+        <title>React Helmet | Home</title>
+      </Helmet>
+      <h1>React Helmet</h1>
+    </>
+  )
+}
+
+export default App
